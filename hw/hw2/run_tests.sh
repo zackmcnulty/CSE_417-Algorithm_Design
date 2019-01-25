5d853ab8ce901893221fecba17ac07652dde58d2
Zachary McNulty <zmcnulty@uw.edu>
2019-01-24 18:31:49 -0800

    added hw2 and start of hw3

--- hw/hw2/run_tests.sh
+++ hw/hw2/run_tests.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-for fn in $(ls biconnectivity-tests/tests/) ; do
+#for fn in $(ls biconnectivity-tests/tests/) ; do
+#	printf  " %s" "$fn"
+#	python3 hw2.py biconnectivity-tests/tests/$fn
+#done
+
+for fn in $(ls bigs/n2048*) ; do
 	printf  " %s" "$fn"
-	python3 hw2.py biconnectivity-tests/tests/$fn
+	python3 hw2.py $fn
 done
